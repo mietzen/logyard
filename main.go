7d623791bc1874c2274559d002d2c11b6633b936
Nils Stein <31704359+mietzen@users.noreply.github.com>
2026-02-21 08:01:36 +0100

    Add --version option

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 	"time"
 )
+
+var version = "dev"
 
 func main() {
 	configPath := flag.String("config", "", "path to config.yaml")
 	alertInterval := flag.Duration("alert-interval", 60*time.Second, "alert evaluation interval")
+	showVersion := flag.Bool("version", false, "print version and exit")
 	flag.Parse()
+
+	if *showVersion {
+		fmt.Println(version)
+		os.Exit(0)
+	}
 
 	cfg, err := LoadConfig(*configPath)
 	if err != nil {
